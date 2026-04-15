8dc71ef5ee938ed82e69c82733fba07e7f0aa29c
unknown <hemanshu264@gmail.com>
2026-04-15 17:15:25 +0530

    Created Readme

--- DES2.py
+++ DES2.py
@@ -1,8 +1,20 @@
 import pandas as pd
-df=pd.read_excel(r"Excel Files\Dataset.xlsx")
+import matplotlib.pyplot as plt
 
-df_num=df.describe()
-print(df_num.columns)
+# df=pd.read_excel(r"Excel Files\Dataset.xlsx")
 
-for i,col in enumerate(df_num.columns):
-    print(i,col)
+# df_num=df.describe()
+# print(df_num.columns)
+
+# for i,col in enumerate(df_num.columns):
+#     print(i,col)
+
+data=pd.read_excel(r"Excel Files\Dataset.xlsx")
+data_num=data.select_dtypes(include="number")
+i=data_num.columns[0:22]
+
+plt.figure(figsize=(10,6))
+plt.scatter(data_num[i[3]],data_num[i[12]])
+plt.xlabel(i[3])
+plt.ylabel(i[12])
+plt.show()
